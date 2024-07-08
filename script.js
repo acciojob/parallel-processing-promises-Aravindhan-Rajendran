515da2d21b1e9a0c6862cb1290d15e5bf6e12ab4
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-08 21:59:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+const promise1= new Promise(function(resolve,reject){
+	resolve(images[0]);
+	reject(console.log(failed()));
+});
+const promise2= new Promise(function(resolve,reject){
+	resolve(images[1]);
+	reject(console.log(failed()));
+});
+const promise3= new Promise(function(resolve,reject){
+	resolve(images[2]);
+	reject(console.log(failed()));
+});
+Promise.all([promise1, promise2, promise3]).then((values) => {
+  console.log(values);
+});
+function failed(){
+	return `Failed to load image's URL:${image.url}`;
+}
